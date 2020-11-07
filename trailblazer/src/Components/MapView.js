422864318a611ab50588b93d79439d9a597556b5
Andrew Sunarto <adsunarto@Andrews-MacBook-Pro.local>
2020-11-07 03:31:17 -0600

    maps

--- trailblazer/src/Components/MapView.js
+++ trailblazer/src/Components/MapView.js
@@ -1,23 +1,25 @@
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
- 
-export class MapContainer extends Component {
+import React, { Component } from 'react';
+import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+class App extends Component {
   render() {
+    const style = {
+      width: 'auto',
+      height: 'auto'
+    }
     return (
-      <Map google={this.props.google} zoom={14}>
- 
-        <Marker onClick={this.onMarkerClick}
-                name={'Current location'} />
- 
-        <InfoWindow onClose={this.onInfoWindowClose}>
-            <div>
-              <h1>{this.state.selectedPlace.name}</h1>
-            </div>
-        </InfoWindow>
-      </Map>
+      <Map 
+        google={this.props.google} 
+        zoom={10}
+        initialCenter={{
+        lat: 35.5496939,
+        lng: -120.7060049
+        }}
+        style={style}
+      />
     );
-  }
+    }
 }
- 
+
 export default GoogleApiWrapper({
-  apiKey: ('AIzaSyDPc33bBrEA6T4kO6gI5wvgYj-Im3faTjM')
-})(MapContainer)
+ apiKey: ('AIzaSyDPc33bBrEA6T4kO6gI5wvgYj-Im3faTjM')
+})(App);
