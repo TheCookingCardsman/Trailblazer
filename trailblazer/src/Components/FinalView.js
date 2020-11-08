b02751e7fecf80371f32006023e504f3b6a13684
Sreevaishnav Deevi <vineteya@gmail.com>
2020-11-08 05:15:10 -0600

    Added individual address capability

--- trailblazer/src/Components/FinalView.js
+++ trailblazer/src/Components/FinalView.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import TrailCard from './TrailCard';
 import CardDeck from 'react-bootstrap/CardDeck';
+import Geocode from 'react-geocode';
 const axios = require('axios');
 
 function FinalView(props) {
@@ -8,38 +9,71 @@ function FinalView(props) {
   const [longitude, setLongitude] = useState(0);
   const [maxDistance, setMaxDistance] = useState(25);
   const [trails, setTrails] = useState([]);
-  const getData = () => {
-    axios.get(`https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=${maxDistance}&key=200967639-5ae34afc9d4778363c950c2067076465`)
-      .then(res => {
-        console.log(res);
-        setTrails(res.data.trails);
-      })
-      .catch(err => {
-        console.log(err);
-      })
-  } 
+  const [address, setAddress] = useState('');
+  const [loadEffect, setLoadEffect] = useState(false);
+  const [addressState, setAddressState] = useState(false);
+  const [getLatitude, setGetLatitude] = useState(false);
+
+  Geocode.setApiKey("AIzaSyAAN2xbYu8DmKyaI_CLnrvevrC2yHNgi3U");
 
   useEffect(() => {
-    if ("geolocation" in navigator) {
-      // setNavigation(true);
-      navigator.geolocation.getCurrentPosition(function(position) {
-      console.log("Latitude is :", position.coords.latitude);
-      setLatitude(position.coords.latitude.toFixed(4));
-      console.log("Longitude is :", position.coords.longitude);
-      setLongitude(position.coords.longitude.toFixed(4));
-      });
-    } else {
-      console.log("Not Available");
+    if (loadEffect == true) {
+      axios.get(`https://www.hikingproject.com/data/get-trails?lat=${latitude}&lon=${longitude}&maxDistance=${maxDistance}&key=200967639-5ae34afc9d4778363c950c2067076465`)
+        .then(res => {
+          console.log(res);
+          setTrails(res.data.trails);
+        })
+        .catch(err => {
+          console.log(err);
+        });
     }
-  })
+  }, [latitude, longitude]);
+
+
+  useEffect(() => {
+    if (address != '') {
+      Geocode.fromAddress(address)
+        .then(response => {
+          const {lat, lng} = response.results[0].geometry.location;
+          setLoadEffect(true);
+          setLatitude(lat); 
+          setLongitude(lng);
+        })
+        .catch((err) => {
+          console.log(err);
+        })
+      }
+  }, [addressState])
+
+  useEffect(() => {
+    if (loadEffect == true) {
+      if ("geolocation" in navigator) {
+        // setNavigation(true);
+        navigator.geolocation.getCurrentPosition(function(position) {
+        console.log("Latitude is :", position.coords.latitude);
+        setLatitude(position.coords.latitude.toFixed(4));
+        console.log("Longitude is :", position.coords.longitude);
+        setLongitude(position.coords.longitude.toFixed(4));
+        });
+      } else {
+        console.log("Not Available");
+      }
+    }
+  }, [loadEffect])
+
+
 
   return (
     <div className="App">
       <header className="App-header">
-        Your Current Latitude and Longitude Coordinates: {latitude}, {longitude}
-        <button onClick={getData} to="/discover">
+        <button onClick={() => setLoadEffect(true)}>
           Find Trails Near You!
         </button>
+        <br />
+        Alternatively, enter any address and we'll give you trails near by: 
+
+        <input type="text" placeholder="Enter Address" onChange={event => setAddress(event.target.value)}></input>
+        <button onClick={() => setAddressState(true)}> Find Trails </button>
 
         <CardDeck>
           {trails.map((info, idx) => {
